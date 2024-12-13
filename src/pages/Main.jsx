db6363889211f63b989ddab9bfe5899d70e3987f
Emre Ünsal <unsalemre111@gmail.com>
2024-12-13 22:37:01 +0300

    update css rule

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,24 +2,14 @@ import React from "react";
 import Button from "../components/Button";
 import RadioInput from "../components/RadioInput";
 import CheckBoxInput from "../components/CheckBoxInput";
+import List from "../components/List";
+import { useSearchContext } from "../context/searchContext";
 
 export default function Main() {
+  const { products } = useSearchContext();
   return (
     <div>
-      Main
-      <Button size="medium">Deneme</Button>
-      <RadioInput
-        onChange={(e) => console.log(e.target.value)}
-        name={"test"}
-        label="New To Old"
-        value="ex1"
-      />
-      <CheckBoxInput
-        onChange={(e) => console.log(e.target.value)}
-        name={"test"}
-        label="New To Old"
-        value="ex1"
-      />
+      <List data={products} />
     </div>
   );
 }
