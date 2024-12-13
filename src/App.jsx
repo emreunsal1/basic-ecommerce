f1340ad4732989be4f1a0c521e89989b108482b2
Emre Ünsal <unsalemre111@gmail.com>
2024-12-14 00:41:54 +0300

    update pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import Header from "./components/Header";
 import Main from "./pages/Main";
 import { BasketContext } from "./context/basketContext";
 import { SearchContext } from "./context/searchContext";
+import { ProductContext } from "./context/productContext";
 
 function App() {
   return (
     <div>
-      <BasketContext>
-        <SearchContext>
-          <Header />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="detail" element={<ProductDetail />} />
-            </Routes>
-          </BrowserRouter>
-        </SearchContext>
-      </BasketContext>
+      <ProductContext>
+        <BasketContext>
+          <SearchContext>
+            <Header />
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="detail" element={<ProductDetail />} />
+              </Routes>
+            </BrowserRouter>
+          </SearchContext>
+        </BasketContext>
+      </ProductContext>
     </div>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import Button from "../components/Button";
-import RadioInput from "../components/RadioInput";
-import CheckBoxInput from "../components/CheckBoxInput";
 import List from "../components/List";
 import { useSearchContext } from "../context/searchContext";
+import BasketSummary from "../components/BasketSummary";
+import Loading from "../components/Loading";
+import Basket from "../components/Basket";
+import { useProductContext } from "../context/productContext";
 
 export default function Main() {
-  const { products, totalPage } = useSearchContext();
+  const { isLoading } = useProductContext();
+  const { searchedProducts } = useSearchContext();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <div>
-      <List data={products} totalPage={totalPage} />
+    <div className="main-page">
+      <div className="middle-wrapper">
+        <List data={searchedProducts} />
+      </div>
+
+      <div className="right-wrapper">
+        <Basket />
+        <BasketSummary />
+      </div>
     </div>
   );
 }
