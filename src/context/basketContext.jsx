202bc2667ddf84571640c3763dfaece9a48d25bd
Emre Ünsal <unsalemre111@gmail.com>
2024-12-13 22:36:42 +0300

    fixed basket context bug

--- src/context/basketContext.jsx
+++ src/context/basketContext.jsx
@@ -8,17 +8,17 @@ const Context = createContext({});
 export function BasketContext({ children }) {
   const [basket, setBasket] = useState([]);
 
-  const addItem = (item) => {
+  const addItem = (productId) => {
     const storageBasket = JSON.parse(STORAGE.getLocal("basket"));
     const foundProduct = storageBasket.find(
-      (product) => product.id === item.id
+      (product) => product.id === productId
     );
     if (foundProduct) {
-      updateItemCount(itemId, foundProduct.count + 1);
+      updateItemCount(productId, foundProduct.count + 1);
       return;
     }
-    storageBasket.push({ id: item.id, count: 1 });
-    STORAGE.setLocal(JSON.stringify(storageBasket));
+    storageBasket.push({ id: productId, count: 1 });
+    STORAGE.setLocal("basket", JSON.stringify(storageBasket));
     setBasket(storageBasket);
   };
 
@@ -27,7 +27,7 @@ export function BasketContext({ children }) {
     const filteredBasket = storageBasket.filter(
       (product) => product.id !== itemId
     );
-    STORAGE.setLocal(JSON.stringify(filteredBasket));
+    STORAGE.setLocal("basket", JSON.stringify(filteredBasket));
     setBasket(filteredBasket);
   };
 
@@ -38,10 +38,10 @@ export function BasketContext({ children }) {
     }
     const storageBasket = JSON.parse(STORAGE.getLocal("basket"));
     const foundIndex = storageBasket.findIndex(
-      (product) => product.id === item.id
+      (product) => product.id === itemId
     );
     storageBasket[foundIndex].count = count;
-    STORAGE.setLocal(JSON.stringify(basket));
+    STORAGE.setLocal("basket", JSON.stringify(storageBasket));
     setBasket(storageBasket);
   };
 
