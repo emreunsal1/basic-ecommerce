e98aa510d3388d14dc474cbdab039935f363b6c2
Emre Ünsal <unsalemre111@gmail.com>
2024-12-13 22:36:11 +0300

    add list and card to main

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Button from "./Button";
+import { useBasketContext } from "../context/basketContext";
 
 export default function Card({ image, name, price, id }) {
+  const { addItem } = useBasketContext();
+
   return (
     <div className="card-wrapper" id={id}>
-      <div className="image-wrapper">{image && <img src={image} />}</div>
-      <div className="name">{name}</div>
-      <div className="price">{price}</div>
+      <div className="content">
+        <div className="image-wrapper">{image && <img src={image} />}</div>
+        <div className="price">{price}</div>
+        <div className="name">{name}</div>
+      </div>
+      <Button onClick={() => addItem(id)} size="medium">
+        Add To Cart
+      </Button>
     </div>
   );
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { useSearchContext } from "../context/searchContext";
+import Card from "./Card";
 
-export default function List() {
-  return <div></div>;
+export default function List({ data }) {
+  return (
+    <div className="list-wrapper">
+      {data.map((item) => {
+        return (
+          <div className="list-item">
+            <Card
+              id={item.id}
+              price={item.price}
+              image={item.image}
+              name={item.name}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
